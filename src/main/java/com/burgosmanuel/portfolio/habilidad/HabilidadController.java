4f457cb8b21525d48f89604c5e3850878ef02096
Manuel <trapecioinv@gmail.com>
2022-07-26 23:54:48 -0300

    PROGRESO: Estructura de archivos para HABILIDAD sin relaciones ni implementacion

---
+++ src/main/java/com/burgosmanuel/portfolio/habilidad/HabilidadController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.burgosmanuel.portfolio.habilidad;
+
+/**
+ *
+ * @author Trapecio
+ */
+public class HabilidadController {
+    
+}
