82f9fca658f1e84514ad2af01e1183777ddff3ec
Manuel <trapecioinv@gmail.com>
2022-07-26 23:51:32 -0300

    PROGRESO: Estructura de archivos para PROYECTO sin relaciones ni implementacion

---
+++ src/main/java/com/burgosmanuel/portfolio/proyecto/Proyecto.java
@@ -0,0 +1,36 @@
+package com.burgosmanuel.portfolio.proyecto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Proyecto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String titulo;
+    private String descripcion;
+    private String img_url;
+    private String demo_url;
+    private String repo_url;
+
+    public Proyecto() {
+    }
+
+    public Proyecto(int id, String titulo, String descripcion, String img_url, String demo_url, String repo_url) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.img_url = img_url;
+        this.demo_url = demo_url;
+        this.repo_url = repo_url;
+    }
+
+}
