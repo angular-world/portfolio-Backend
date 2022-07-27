f7a6d2109b120cb762019580ebe497dc1ea3a6b5
Manuel <trapecioinv@gmail.com>
2022-07-26 23:42:23 -0300

    PROGRESO: Estructura de archivos para SECCION sin relaciones ni implementacion

---
+++ src/main/java/com/burgosmanuel/portfolio/seccion/Seccion.java
@@ -0,0 +1,22 @@
+package com.burgosmanuel.portfolio.seccion;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Seccion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String titulo;
+    private String descripcion;
+}
