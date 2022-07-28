fbfa4378f53d0d1c4853876234df4dcce8f5c463
Manuel <trapecioinv@gmail.com>
2022-07-28 01:04:50 -0300

    CRUD Finalizado en Proyectos

--- src/main/java/com/burgosmanuel/portfolio/proyecto/IProyectoService.java
+++ src/main/java/com/burgosmanuel/portfolio/proyecto/IProyectoService.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.burgosmanuel.portfolio.proyecto;
 
-/**
- *
- * @author Trapecio
- */
+import java.util.List;
+
+
 public interface IProyectoService {
+    public void agregarProyecto(Proyecto proj);
     
+    public void editarProyecto(int id, Proyecto datosProyecto);
+    
+    public Proyecto buscarProyecto(int id);
+    
+    public List<Proyecto> listarProyectos();
+    
+    public void borrarProyecto(int id);  
 }
