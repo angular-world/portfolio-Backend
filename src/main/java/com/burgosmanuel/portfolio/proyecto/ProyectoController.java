bd5b4697a5eaf9ca0b4e25bafc74328582de4344
Manuel <trapecioinv@gmail.com>
2022-07-28 01:04:50 -0300

    CRUD Finalizado en Proyectos

--- src/main/java/com/burgosmanuel/portfolio/proyecto/ProyectoController.java
+++ src/main/java/com/burgosmanuel/portfolio/proyecto/ProyectoController.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.burgosmanuel.portfolio.proyecto;
 
-/**
- *
- * @author Trapecio
- */
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@RestController
 public class ProyectoController {
-    
+
+    @Autowired
+    ProyectoService service;
+
+    @PostMapping("/proyectos/agregar")
+    public void agregarProyecto(@RequestBody Proyecto proj) {
+        service.agregarProyecto(proj);
+    }
+
+    @PutMapping("/proyectos/editar/{id}")
+    public void editarProyecto(@PathVariable int id, @RequestBody Proyecto datosProyecto) {
+        service.editarProyecto(id, datosProyecto);
+    }
+
+    @GetMapping("/proyectos/{id}")
+    public Proyecto buscarProyecto(@PathVariable int id) {
+        return service.buscarProyecto(id);
+    }
+
+    @GetMapping("/proyectos")
+    public List<Proyecto> listarProyectos() {
+        return service.listarProyectos();
+    }
+
+    @DeleteMapping("/proyectos/eliminar/{id}")
+    public void borrarProyecto(@PathVariable int id) {
+        service.borrarProyecto(id);
+    }
 }
--- src/main/java/com/burgosmanuel/portfolio/proyecto/ProyectoService.java
+++ src/main/java/com/burgosmanuel/portfolio/proyecto/ProyectoService.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.burgosmanuel.portfolio.proyecto;
 
-/**
- *
- * @author Trapecio
- */
-public class ProyectoService {
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProyectoService implements IProyectoService {
+
+    @Autowired
+    ProyectoRepository repo;
+    
+    @Override
+    public void agregarProyecto(Proyecto proj) {
+        repo.save(proj);
+    }
+    
+    @Override
+    public void editarProyecto(int id, Proyecto datosProyecto) {
+        Proyecto proj = buscarProyecto(id);
+        proj = datosProyecto;
+        repo.save(proj);
+    }
+    
+    @Override
+    public Proyecto buscarProyecto(int id) {
+        return repo.findById(id).orElse(null);
+    }
+    
+    @Override
+    public List<Proyecto> listarProyectos() {
+        return repo.findAll();
+    }
+    
+    @Override
+    public void borrarProyecto(int id) {
+        repo.deleteById(id);
+    }
     
 }
