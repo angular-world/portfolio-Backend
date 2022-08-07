3f976b73bdbed6569f4e27df6658e2ab614a19ce
Manuel <trapecioinv@gmail.com>
2022-08-07 02:11:55 -0300

    SISTEMA DE LOGIN -> Permitidos los metodos GET sin autenticar. FIXED Jwt Expiration Time era demasiado corto.

--- src/main/java/com/burgosmanuel/portfolio/persona/PersonaController.java
+++ src/main/java/com/burgosmanuel/portfolio/persona/PersonaController.java
@@ -1,32 +1,37 @@
 package com.burgosmanuel.portfolio.persona;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
+@RequestMapping("/personas")
 public class PersonaController {
 
     @Autowired
     PersonaService service;
 
-    @GetMapping("/personas/{id}")
+    @GetMapping("/{id}")
     public Persona buscarPersona(@PathVariable int id) {
         return service.buscarPersona(id);
     }
 
-    @PostMapping("personas/crear")
+    @PostMapping("/crear")
+        @PreAuthorize("hasRole('ADMIN')")
     public void crearPersona(@RequestBody Persona pers) {
         service.crearPersona(pers);
     }
 
-    @PutMapping("personas/editar/{id}")
+    @PutMapping("/editar/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public void editarPersona(@PathVariable int id, @RequestBody Persona datosPersona) {
         service.editarPersona(id, datosPersona);
     }
--- src/main/java/com/burgosmanuel/portfolio/proyecto/ProyectoController.java
+++ src/main/java/com/burgosmanuel/portfolio/proyecto/ProyectoController.java
@@ -2,6 +2,7 @@ package com.burgosmanuel.portfolio.proyecto;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,36 +10,41 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
+@RequestMapping("/proyectos")
 public class ProyectoController {
 
     @Autowired
     ProyectoService service;
 
-    @PostMapping("/proyectos/agregar")
+    @PostMapping("/agregar")
+    @PreAuthorize("hasRole('ADMIN')")
     public void agregarProyecto(@RequestBody Proyecto proj) {
         service.agregarProyecto(proj);
     }
 
-    @PutMapping("/proyectos/editar/{id}")
+    @PutMapping("/editar/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public void editarProyecto(@PathVariable int id, @RequestBody Proyecto datosProyecto) {
         service.editarProyecto(id, datosProyecto);
     }
 
-    @GetMapping("/proyectos/{id}")
+    @GetMapping("/{id}")
     public Proyecto buscarProyecto(@PathVariable int id) {
         return service.buscarProyecto(id);
     }
 
-    @GetMapping("/proyectos")
+    @GetMapping("/listar")
     public List<Proyecto> listarProyectos() {
         return service.listarProyectos();
     }
 
-    @DeleteMapping("/proyectos/eliminar/{id}")
+    @DeleteMapping("/eliminar/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public void borrarProyecto(@PathVariable int id) {
         service.borrarProyecto(id);
     }
