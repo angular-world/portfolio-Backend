11ea076f68753144cee0bad78b18188998e0a1ae
Manuel <trapecioinv@gmail.com>
2022-08-10 01:24:24 -0300

    Habilitar CORS global

--- src/main/java/com/burgosmanuel/portfolio/habilidad/HabilidadController.java
+++ src/main/java/com/burgosmanuel/portfolio/habilidad/HabilidadController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin
 @RestController
 @RequestMapping("/habilidades")
 public class HabilidadController {
