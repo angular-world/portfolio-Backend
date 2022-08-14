42558030f8b07a5f97f40198c8b73508da409399
Manuel <trapecioinv@gmail.com>
2022-08-13 22:53:06 -0300

    Persona Service -> Modificaciones para asignar un User al editar. No necesita DTO.

--- src/main/java/com/burgosmanuel/portfolio/persona/PersonaService.java
+++ src/main/java/com/burgosmanuel/portfolio/persona/PersonaService.java
@@ -1,5 +1,6 @@
 package com.burgosmanuel.portfolio.persona;
 
+import com.burgosmanuel.portfolio.security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +9,8 @@ public class PersonaService implements IPersonaService {
 
     @Autowired
     PersonaRepository repo;
+    @Autowired
+    UserRepository userRepo;
 
     @Override
     public Persona buscarPersona(Long id) {
@@ -23,6 +26,8 @@ public class PersonaService implements IPersonaService {
     public void editarPersona(Long id, Persona datosPersona) {
         Persona pers = buscarPersona(id);
         pers = datosPersona;
+        pers.setUser(userRepo.findById(id).orElse(null));
+        System.out.println(pers.toString());
         repo.save(pers);
     }
 }
