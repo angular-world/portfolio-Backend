139ca96fdff0bb86bbf4552d4ec9c5d86689b8c7
Manuel <trapecioinv@gmail.com>
2022-09-14 02:37:25 -0300

    Nueva DB - AWS

--- src/main/java/com/burgosmanuel/portfolio/PortfolioApplication.java
+++ src/main/java/com/burgosmanuel/portfolio/PortfolioApplication.java
@@ -1,5 +1,6 @@
 package com.burgosmanuel.portfolio;
 
+import com.burgosmanuel.portfolio.security.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
