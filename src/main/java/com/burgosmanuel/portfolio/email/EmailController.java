82dc46a1f74dde4e22847849417a93ee38e1d1fc
Manuel <trapecioinv@gmail.com>
2022-09-15 14:29:40 -0300

    CORS 2.0

--- src/main/java/com/burgosmanuel/portfolio/email/EmailController.java
+++ src/main/java/com/burgosmanuel/portfolio/email/EmailController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
+@CrossOrigin(origins = "https://burgosmanuel.ar")
 @RestController
 public class EmailController {
     @Autowired
--- src/main/java/com/burgosmanuel/portfolio/experiencia/ExperienciaController.java
+++ src/main/java/com/burgosmanuel/portfolio/experiencia/ExperienciaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
+@CrossOrigin(origins = "https://burgosmanuel.ar")
 @RestController
 @RequestMapping("/experiencia")
 @Tag(name = "Experiencia", description = "Se encarga de las operaciones a realizar en los elementos de Experiencia.")
